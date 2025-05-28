5a169d6974288adcdcbc6f19c5e07af2f1048163
Google Team Member <genai-sdk-bot@google.com>
2025-05-28 14:37:13 -0700

    Add download object as json function in the download service.
    PiperOrigin-RevId: 764421398

--- src/app/core/services/download.service.ts
+++ src/app/core/services/download.service.ts
@@ -35,4 +35,23 @@ export class DownloadService {
       throw error;
     }
   }
+
+  downloadObjectAsJson(data: object, filename = 'session.json'): void {
+    const jsonString = JSON.stringify(data, null, 2);
+
+    const blob = new Blob([jsonString], {type: 'application/json'});
+
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }
 }

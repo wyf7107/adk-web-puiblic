22e7485f6e847167eeef2efe07bcde7167b0b32e
jfzeng00 <jfzeng00@gmail.com>
2025-07-29 15:59:13 -0700

    feat: Use mat-tree for sub agents list
    feat: Use mat-tree for sub agents list

--- src/app/components/builder-tabs/builder-tabs.component.ts
+++ src/app/components/builder-tabs/builder-tabs.component.ts
@@ -15,19 +15,23 @@
  * limitations under the License.
  */
 
-import { Component, inject, ViewChild, signal } from '@angular/core';
+import { Component, inject, ViewChild, signal, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { AgentNode, ToolNode } from '../../core/models/AgentBuilder';
 import { AgentBuilderService } from '../../core/services/agent-builder.service';
-import {JsonEditorComponent} from '../json-editor/json-editor.component'
+import {JsonEditorComponent} from '../json-editor/json-editor.component';
+import { BehaviorSubject } from 'rxjs';
+import { MatTree } from '@angular/material/tree'; // Import MatTree component type
 
 @Component({
   selector: 'app-builder-tabs',
   templateUrl: './builder-tabs.component.html',
   styleUrl: './builder-tabs.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false
 })
 export class BuilderTabsComponent {
   @ViewChild(JsonEditorComponent) jsonEditorComponent!: JsonEditorComponent;
+  @ViewChild(MatTree) matTree!: MatTree<AgentNode>;
   protected toolArgsString = signal('');
   editingToolArgs = signal(false);
 
@@ -40,6 +44,12 @@ export class BuilderTabsComponent {
     instruction: '',
     sub_agents: []
   };
+
+  treeDataSource = new BehaviorSubject<AgentNode[]>([]);
+
+  childrenAccessor = (node: AgentNode) => node.sub_agents ?? [];
+
+  hasChild = (_: number, node: AgentNode) => (!!node.sub_agents && node.sub_agents.length > 0) || !!node.isRoot;
 
   // Agent configuration options
   isRootAgentEditable: boolean = true;
@@ -97,12 +107,27 @@ export class BuilderTabsComponent {
   ]);
   protected header = 'Select an agent or tool to edit'
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
     this.agentBuilderService.getSelectedNode().subscribe(node => {
       this.agentConfig = node;
       if (node) {
         this.header = 'Agent configuration';
-      }
+        const oldTreeData = this.treeDataSource.value;
+        this.treeDataSource.next([]);
+        this.treeDataSource.next([node]);
+        this.cdr.detectChanges();
+        setTimeout(() => {
+          // Code to execute after 100 milliseconds
+          // Open the root node by default
+          if (oldTreeData.length == 0) {
+            this.matTree.expand(node);
+          }
+
+          // Refresh the tree node once data gets updated
+          this.matTree.toggle(node);
+          this.matTree.toggle(node);
+        }, 100);
+       }
     });
 
     this.agentBuilderService.getSelectedTool().subscribe(tool => {
@@ -166,5 +191,9 @@ export class BuilderTabsComponent {
       }
       this.toolArgsString.set(JSON.stringify(this.selectedTool.args, null, 2));
     }
+  }
+
+  trackByFn(index: number, node: AgentNode): string {
+    return node.name;
   }
 }

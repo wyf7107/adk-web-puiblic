4aa51bdd1cd466dde3c53c0c2c5fda552d49b22b
Ariz Chang <arizchang@google.com>
2025-07-30 17:21:20 -0700

    fix scrollbar issue and remove tool config form from agent config tab

--- src/app/components/builder-tabs/builder-tabs.component.ts
+++ src/app/components/builder-tabs/builder-tabs.component.ts
@@ -134,11 +134,6 @@ export class BuilderTabsComponent {
 
     this.agentBuilderService.getSelectedTool().subscribe(tool => {
       this.selectedTool = tool;
-      if (tool) {
-        this.header = 'Tool configuration';
-      } else {
-        this.header = 'Select an agent or tool to edit';
-      }
       this.cdr.detectChanges();
     });
 

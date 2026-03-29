f1acb20c76504131b27066dc421d0b9c724cff7f
Bo Yang <ybo@google.com>
2026-03-29 16:38:36 -0700

    feat: add paginator component to trace details for span navigation

--- src/app/components/trace-tab/trace-tab.component.ts
+++ src/app/components/trace-tab/trace-tab.component.ts
@@ -14,14 +14,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {ChangeDetectionStrategy, Component, inject, Input, output, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Input, output, signal, Injectable} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatPaginator, MatPaginatorIntl, PageEvent} from '@angular/material/paginator';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {NgxJsonViewerModule} from 'ngx-json-viewer';
 
 import {TRACE_SERVICE} from '../../core/services/interfaces/trace';
+
+@Injectable()
+export class SpanPaginatorIntl extends MatPaginatorIntl {
+  override nextPageLabel = 'Next Span';
+  override previousPageLabel = 'Previous Span';
+  override firstPageLabel = 'First Span';
+  override lastPageLabel = 'Last Span';
+
+  override getRangeLabel = (page: number, pageSize: number, length: number) => {
+    if (length === 0) {
+      return `Span 0 of 0`;
+    }
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    return `Span ${startIndex + 1} of ${length}`;
+  };
+}
 
 @Component({
   changeDetection: ChangeDetectionStrategy.Default,
@@ -30,7 +48,10 @@ import {TRACE_SERVICE} from '../../core/services/interfaces/trace';
   styleUrl: './trace-tab.component.scss',
   standalone: true,
   imports: [
-    MatButtonModule, MatIconModule, MatTooltipModule, NgxJsonViewerModule
+    MatButtonModule, MatIconModule, MatTooltipModule, NgxJsonViewerModule, MatPaginator
+  ],
+  providers: [
+    { provide: MatPaginatorIntl, useClass: SpanPaginatorIntl }
   ]
 })
 export class TraceTabComponent {
@@ -59,6 +80,19 @@ export class TraceTabComponent {
     const span = this.traceData.find(s => String(s.span_id) === String(id));
     if (span) {
       this.traceService.selectedRow(span);
+    }
+  }
+
+  get selectedSpanIndex(): number | undefined {
+    const span = this.selectedSpan();
+    if (!span) return undefined;
+    const index = this.traceData.findIndex(s => s.span_id === span.span_id);
+    return index === -1 ? undefined : index;
+  }
+
+  onPage(event: PageEvent) {
+    if (event.pageIndex >= 0 && event.pageIndex < this.traceData.length) {
+      this.traceService.selectedRow(this.traceData[event.pageIndex]);
     }
   }
   
